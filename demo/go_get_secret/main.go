39191bc33302c3d983c7050c46adf19fcc2e7320
Julien Briault <julien.briault@restosducoeur.org>
2025-04-13 10:56:48 +0200

    Initial commit

--- demo/go_get_secret/main.go
+++ demo/go_get_secret/main.go
@@ -2,12 +2,21 @@ package main
 
 import (
 	"context"
+	"fmt"
+	"html/template"
 	"log"
+	"net/http"
 	"os"
+	"sync"
 	"time"
 
 	infisical "github.com/infisical/go-sdk"
 	"github.com/joho/godotenv"
+)
+
+var (
+	logs      []string
+	logsMutex sync.Mutex
 )
 
 func main() {
@@ -31,21 +40,76 @@ func main() {
 		log.Fatalf("Authentication failed: %v", err)
 	}
 
-	for {
-		apiKeySecret, err := client.Secrets().Retrieve(infisical.RetrieveSecretOptions{
-			SecretKey:   "API_KEY",
-			Environment: "dev",
-			ProjectID:   projectID,
-			SecretPath:  "/",
-		})
+	go func() {
+		for {
+			apiKeySecret, err := client.Secrets().Retrieve(infisical.RetrieveSecretOptions{
+				SecretKey:   "API_KEY",
+				Environment: "dev",
+				ProjectID:   projectID,
+				SecretPath:  "/",
+			})
 
-		if err != nil {
-			log.Printf("Error retrieving API Key Secret: %v", err)
-		} else {
-			log.Printf("(full) API Key Secret: %v", apiKeySecret)
-			// log.Printf("(only) API Key Secret: %v", apiKeySecret.SecretValue)
+			if err != nil {
+				addLog("Error retrieving API Key Secret: %v", err)
+			} else {
+				addLogWithTimestamp("(only) API Key Secret: %v", apiKeySecret.SecretValue)
+			}
+
+			time.Sleep(1 * time.Second)
 		}
+	}()
 
-		time.Sleep(2 * time.Second)
+	http.HandleFunc("/", handleLogs)
+	http.HandleFunc("/logs", handleLogs)
+	http.HandleFunc("/clear", handleClearLogs)
+	log.Println("Starting server on :8084")
+	log.Fatal(http.ListenAndServe("localhost:8084", nil))
+}
+
+func addLog(format string, args ...interface{}) {
+	logsMutex.Lock()
+	defer logsMutex.Unlock()
+	logMessage := fmt.Sprintf(format, args...)
+	logs = append(logs, logMessage)
+}
+
+func addLogWithTimestamp(format string, args ...interface{}) {
+	logsMutex.Lock()
+	defer logsMutex.Unlock()
+	timestamp := time.Now().Format(time.RFC1123)
+	logMessage := fmt.Sprintf("[%s] "+format, append([]interface{}{timestamp}, args...)...)
+	logs = append(logs, logMessage)
+}
+
+func handleLogs(w http.ResponseWriter, r *http.Request) {
+	logsMutex.Lock()
+	defer logsMutex.Unlock()
+
+	tmpl := template.Must(template.New("logs").Parse(`
+		<!DOCTYPE html>
+		<html>
+		<head>
+			<title>Logs Infisical - Get secrets with Go SDK - Demo Devoxx FR</title>
+			<meta http-equiv="refresh" content="1">
+		</head>
+		<body>
+			<h1>Logs Infisical - Get secrets with Go SDK</h1>
+			<form action="/clear" method="post">
+				<button type="submit">Clear Logs</button>
+			</form>
+			<pre>{{range .}}{{.}}<br>{{end}}</pre>
+		</body>
+		</html>
+	`))
+
+	tmpl.Execute(w, logs)
+}
+
+func handleClearLogs(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodPost {
+		logsMutex.Lock()
+		defer logsMutex.Unlock()
+		logs = []string{}
+		http.Redirect(w, r, "/", http.StatusSeeOther)
 	}
 }
